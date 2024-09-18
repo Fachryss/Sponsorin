bacfb6d282be0b3757e0ddb868ba76ddb3e19054
Haza Nasrullah <hazanasrullah@gmail.com>
2024-09-18 15:58:21 +0700

    feat: Add AIGenerate page with empty container

---
+++ lib/page/AIGenerate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AIGenerate extends StatefulWidget {
+  const AIGenerate({super.key});
+
+  @override
+  State<AIGenerate> createState() => _AIGenerateState();
+}
+
+class _AIGenerateState extends State<AIGenerate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
