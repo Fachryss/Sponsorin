a34425fe5fc9705db78337e0ff1e51005f68bf30
Haza Nasrullah <hazanasrullah@gmail.com>
2024-10-10 17:00:26 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:sponsorin/page%20EO/homepage.dart';
 import 'package:sponsorin/page%20EO/informasi-usaha.dart';
 import 'package:sponsorin/style/textstyle.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
   runApp(MyApp());
 }
 
